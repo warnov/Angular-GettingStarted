d8d7a4820c52ee144b5d234f80ba831ddea6498f
warnov <warnov@gmail.com>
2017-08-18 20:29:00 -0500

    antes mogotes

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core'
 @Component({
-  selector:'nq-root',
+  selector:'pm-root',
   template:`
     <div><h1>{{pageTitle}}</h1>
       <div>My First Component</div>
